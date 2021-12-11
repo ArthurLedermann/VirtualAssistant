49a95573be09ae2b375c9d1e9437d9b0bf3c1900
ArthurLedermann <91742160+ArthurLedermann@users.noreply.github.com>
2021-12-11 16:17:42 +0100

    Delete Alpha Version (Broken) directory

--- Alpha Version (Broken)/he_ho.py
+++
@@ -1,7 +0,0 @@
-import playsound
-
-def eh_oh():
-    playsound.playsound(r"c:\Users\arthu\desktop\Alice Project\eh_oh.mp3")
-
-def pompom():
-    playsound.playsound(r"c:\Users\arthu\desktop\Alice Project\pompompom.mp3")
